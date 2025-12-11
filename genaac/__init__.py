80d317ebceeffc46ab981ee568358398ed8d491a
HaSha-Laptop <whitebluej@kaist.ac.kr>
2025-12-11 14:29:53 +0900

    tokenizing

--- genaac/__init__.py
+++ genaac/__init__.py
@@ -4,3 +4,4 @@ from .config import CONFIG, ROOT_DIR
 
 from .aac_from_image import aac_from_image, aac_from_image_parallel
 from .aac_from_keyword import aac_from_keyword, aac_from_keyword_parallel
+from .tokenizing import Token, tokenizing
